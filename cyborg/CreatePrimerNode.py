4b44577d30ec0a2ffb40c4a66ad768c9f8c01034
Isaac Luo <yishaluo@gmail.com>
2014-06-11 10:40:54 +0100

    fixed bug of A.gossypii

--- cyborg/CreatePrimerNode.py
+++ cyborg/CreatePrimerNode.py
@@ -4,35 +4,37 @@ import json
 import Nodes
 
 def carver2(self):
-	para = json.loads(self.Feature)
-	sequence = para["Sequence"]
-	chromosome = para["Chromosome"]
-	checkBoundary = para["CheckBoundary"]
-	featureName = para["FeatureName"]
-	if para["Species"] == "S.cerevisiae":
-		species = "SC"
-	elif para["Species"] == "A.thaliana":
-		species = "AT"
-	else:
-		species = self.Species
-	TargetPart = para["TargetPart"]
+    para = json.loads(self.Feature)
+    sequence = para["Sequence"]
+    chromosome = para["Chromosome"]
+    checkBoundary = para["CheckBoundary"]
+    featureName = para["FeatureName"]
+    if para["Species"] == "S.cerevisiae":
+        species = "SC"
+    elif para["Species"] == "A.thaliana":
+        species = "AT"
+    elif self.Species == "A.gossypii":
+        species = "AG"
+    else:
+        species = self.Species
+    TargetPart = para["TargetPart"]
 
 
-	std = self.Standard
-	stdDict = {'promotor5':'GGTC','promotor3':'CATC','orf5':'GATG','orf3':'GCTA','terminator5':'TAGC','terminator3':'GAGG'}
-	cStr5 = stdDict[TargetPart+'5']
-	cStr3 = stdDict[TargetPart+'3']
-	srcStr5 = str(sequence)
-	srcStr3 = str(srcStr5)[::-1]
-	srcStr3 = srcStr3.translate(string.maketrans("AaTtCcGg","TtAaGgCc"))
-	primerLength = int(self.PrimerLength)
-	if std == "GoldenGate":
-		self.Primer5 = ("GGTCTCg"+cStr5+srcStr5)[0:primerLength]
-		self.Primer3 = ("GGTCTCg"+cStr3+srcStr3)[0:primerLength]
-	else:
-		self.Primer5 = "not support yet"
-	
-	self.Report = "http://genomecarver.cailab.org/carve/?species=%s&chromosomeName=%s&geneName=%s&checkBoundary=%s&standard=%s&carvePara=%s"%(species,chromosome,featureName,checkBoundary,self.Standard,TargetPart)
+    std = self.Standard
+    stdDict = {'promotor5':'GGTC','promotor3':'CATC','orf5':'GATG','orf3':'GCTA','terminator5':'TAGC','terminator3':'GAGG'}
+    cStr5 = stdDict[TargetPart+'5']
+    cStr3 = stdDict[TargetPart+'3']
+    srcStr5 = str(sequence)
+    srcStr3 = str(srcStr5)[::-1]
+    srcStr3 = srcStr3.translate(string.maketrans("AaTtCcGg","TtAaGgCc"))
+    primerLength = int(self.PrimerLength)
+    if std == "GoldenGate":
+        self.Primer5 = ("GGTCTCg"+cStr5+srcStr5)[0:primerLength]
+        self.Primer3 = ("GGTCTCg"+cStr3+srcStr3)[0:primerLength]
+    else:
+        self.Primer5 = "not support yet"
+    
+    self.Report = "http://genomecarver.cailab.org/carve/?species=%s&chromosomeName=%s&geneName=%s&checkBoundary=%s&standard=%s&carvePara=%s"%(species,chromosome,featureName,checkBoundary,self.Standard,TargetPart)
 
 carver2(self)
 
--- cyborg/GetSequenceNode.py
+++ cyborg/GetSequenceNode.py
@@ -4,28 +4,28 @@ import json
 import Nodes
 
 def getSequence(feature,cursor):
-	cursor.execute("SELECT chromosome FROM neochromosome_chromosome WHERE id = %d"%(feature[1]))
-	seq = cursor.fetchone()[0]
-	if feature[4] == "+":
-		return str(seq[feature[2]-1:feature[3]])
-	elif feature[4] == "-":
-		return str(seq[feature[2]-1:feature[3]][::-1]).translate(string.maketrans("AaTtCcGg","TtAaGgCc"))
-	else:
-		raise "neither + nor -"
+    cursor.execute("SELECT chromosome FROM neochromosome_chromosome WHERE id = %d"%(feature[1]))
+    seq = cursor.fetchone()[0]
+    if feature[4] == "+":
+        return str(seq[feature[2]-1:feature[3]])
+    elif feature[4] == "-":
+        return str(seq[feature[2]-1:feature[3]][::-1]).translate(string.maketrans("AaTtCcGg","TtAaGgCc"))
+    else:
+        raise "neither + nor -"
 
 def carver2(self):
-	connection = psycopg2.connect(database="neochromosome", user="cyborg", password = "adsk2cailab", host="git.cailab.org", port="5432")
-	
-	cursor = connection.cursor()
-	
-	para = json.loads(self.Selection)
-	feature  = para["CarvedFeature"]
-	
-	for i in range(0,4):
-		feature[i] = int(feature[i])
-	
-	self.Sequence = getSequence(feature,cursor)
-	para["Sequence"]=self.Sequence
-	self.Feature = json.dumps(para)
+    connection = psycopg2.connect(database="neochromosome", user="cyborg", password = "adsk2cailab", host="git.cailab.org", port="5432")
+    
+    cursor = connection.cursor()
+    
+    para = json.loads(self.Selection)
+    feature  = para["CarvedFeature"]
+    
+    for i in range(0,4):
+        feature[i] = int(feature[i])
+    
+    self.Sequence = getSequence(feature,cursor)
+    para["Sequence"]=self.Sequence
+    self.Feature = json.dumps(para)
 
 carver2(self)
